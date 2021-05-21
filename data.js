4f241767c3834b3eed73279a103162bf084b1e1b
sudhanshu786kumar <46154168+sudhanshu786kumar@users.noreply.github.com>
2021-05-21 12:54:23 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+const data=[
+    {   id:1,
+        imgsrc:"https://picsum.photos/300/300" ,
+        title:"original netflix series",
+         link:"https://picsum.photos/200/300",
+    },
+    {    id:2,
+        imgsrc:"https://picsum.photos/400/300" ,
+        title:"original netflix series",
+         link:"https://picsum.photos/200/300",
+    },{ id:3,
+        imgsrc:"https://picsum.photos/420/300",
+        title:"original netflix series",
+         link:"https://picsum.photos/200/300",},
+]
+ 
+export default data;
